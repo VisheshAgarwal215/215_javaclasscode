c46c5b1f964687a6763b88b23690a5aac5f851de
VisheshAgarwal215 <vishesh.2428cseaiml1@kiet.edu>
2025-08-23 19:12:00 +0530

    Update HRDepartment.java

--- KIET/HRDepartment.java
+++ KIET/HRDepartment.java
@@ -4,7 +4,7 @@ public class HRDepartment {
     public static void main(String[] args) {
         EmployeeKiet ek = new EmployeeKiet();
 
-        ek.Emp_name="kumar";
+        ek.Emp_name="Agarwal";
         ek.Emp_id=1;
         ek.Organisation="kiet group";
         ek.employeeDetails();
